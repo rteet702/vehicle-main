cdf66f05b98fff5a1827bdcf36658cde3451d18f
Robert Teets <robertteets@Roberts-MacBook-Pro.local>
2023-07-03 14:30:27 -0600

    chore: attempting to fix deployment issue.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,21 +1,3 @@
-"use client";
-
-import { deleteCookie, hasCookie } from "cookies-next";
-import { useRouter } from "next/navigation";
-
 export default function Page() {
-    const router = useRouter();
-
-    if (!hasCookie("userId")) router.push("/");
-
-    const handleLogout = () => {
-        deleteCookie("userId");
-        router.push("/");
-    };
-
-    return (
-        <main>
-            <button onClick={handleLogout}>Logout</button>
-        </main>
-    );
+    return <main></main>;
 }
